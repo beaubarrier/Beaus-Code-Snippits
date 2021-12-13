07038680be0860433c3c2346852df26cab0cbffd
Beau Barrier <beaubarrier@gmail.com>
2021-12-13 14:56:55 -0800

    Added detectPangram for strings

--- For Arrays/compareArraysRemoveMatches.js
+++ For Arrays/compareArraysRemoveMatches.js
@@ -1,6 +1,12 @@
-array1 = array1.filter(function (val) {
-  return array2.indexOf(val) == -1;
-});
+// array1 = array1.filter(function (val) {
+//   return array2.indexOf(val) == -1;
+// });
 // Or with ES6:
 
-array1 = array1.filter((val) => !array2.includes(val));
+function test() {
+  let array2 = [1, 2, 3, 4, 5, 6];
+  let array1 = [1, 7, 8, 9];
+  array1 = array1.filter((val) => !array2.includes(val));
+  console.log(array1);
+}
+test();
