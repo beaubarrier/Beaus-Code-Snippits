a5164e5c4aeb57a789371e1594667c05cf0de706
Beau Barrier <beaubarrier@gmail.com>
2021-12-14 14:50:00 -0800

    Added snip for sorting strings containing alphanumeric words

---
+++ For Strings/sortWordsInNumericalOrder.js
@@ -0,0 +1,10 @@
+//Useful if sorting through filenames with letters and numbers
+
+function order(words) {
+  return words
+    .split(" ")
+    .sort(function (a, b) {
+      return a.match(/\d/) - b.match(/\d/);
+    })
+    .join(" ");
+}
