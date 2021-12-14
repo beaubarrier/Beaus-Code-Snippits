7ef2b8a94a659ec392fbc0f1395763dde3baa7d8
Beau Barrier <beaubarrier@gmail.com>
2021-12-14 10:58:04 -0800

    added kandanes algo

---
+++ For Numbers/KandanesAlgo(maxSubarrSum).js
@@ -0,0 +1,17 @@
+const maxSubArray = (nums) => {
+  // initiate two variable, maxSum for total max, sum for current max
+  let maxSum = 0; // Using 0 instead of -Infinity allows to work on empty arrays
+  let currentSum = 0;
+  // iterate through the nums, store sub-problems result
+  for (let i = 0; i < nums.length; i++) {
+    //cumulating answers to the top
+
+    //compare currentSum add current number
+    //with current number and store the maximum value
+    currentSum = Math.max(nums[i], currentSum + nums[i]);
+
+    //compare maxSum with currentSum and store the greater value
+    maxSum = Math.max(currentSum, maxSum);
+  }
+  return maxSum;
+};
